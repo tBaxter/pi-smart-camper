bb47facfc58a34c77757417f1c487b5fe14cf00b
TBaxter <mail.baxter@gmail.com>
2020-03-15 14:02:40 -0500

    Added some motion sensing bits from prior efforts. Will require cleanup

---
+++ monitoring/motion_sensing.py
@@ -0,0 +1,77 @@
+import smtplib, ssl
+from datetime import datetime
+from time import sleep
+from threading import Thread
+
+# Imports to allow us to email. 
+# We may not need this in the future.
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+
+EMAIL_ADDR: "mail.baxter@gmail.com"
+EMAIL_SUBJECT: "Motion detected by RPi"
+
+
+def send_email(img_path):
+    """
+    Should send an email, when properly configured with an SMTP server.
+    May not be needed if we can get modem working correctly.
+    """
+    msg = MIMEMultipart()
+    msg['Subject'] = EMAIL_SUBJECT
+    msg['To'] = EMAIL_ADDR
+    msg['From'] = EMAIL_ADDR
+    msg.preamble ="Message sent from RPi "
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(open(img_path, 'rb').read())
+    encoders.encode_base64(part)
+    part.add_header(
+        'Content-Disposition',
+        'attachment; filename="motion_pic.jpg"'
+    )
+    msg.attach(part)
+    # to do: figure this bit out in a way that's
+    # sharable and reusable
+    try:
+        s =smtplib.SMTP('some smtp server', 587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        
+        s.login(
+            user = 'smtp service user',
+            password = 'smtp service pass'
+        )
+        s.sendmail(email_addr, email_addr, msg.as_string())
+        s.quit()
+    except Exception as error:
+        # To do: log this error
+        print("Error: %s" % error)
+
+def motion_callback(cam):
+    """
+    Just create and store an image, using the time as filename,
+    then send it out.
+    """
+    img_path = "/home/pi/camphotos/%s.jpg" % datetime.now().date()
+    cam.capture(img_path)
+    send_email(img_path)
+
+def start_camera():
+    """
+    Configure and start the camera up on demand.
+    """
+    SENSOR_PIN = 7
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SENSOR_PIN, GPIO.IN)
+    cam = PiCamera()
+    cam.resolution = (800, 600)
+    img_path = "/home/pi/camphotos/%s.jpg" % datetime.now().date()
+    print("Motion detection initalizing...")
+    GPIO.add_event_detect(SENSOR_PIN, GPIO.RISING, callback=lambda x: motion_callback(cam))
---
+++ webapp/app.py
@@ -0,0 +1,52 @@
+import datetime
+from threading import Thread
+
+from flask import Flask, render_template, redirect
+
+import RPi.GPIO as GPIO
+
+from camera import start_camera
+
+SENSOR_PIN = 7
+
+# By default these should be off. We'll set later
+cam_thread = None
+message = None
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   global cam_thread
+   global message
+
+   templateData = {
+      'title' : 'HELLO!',
+      'message': message,
+      'cam_thread': cam_thread
+   }
+   return render_template('index.html', **templateData)
+
+@app.route("/camera/<action>")
+def action(action):
+    """ Handles turning camera off and on based on user action. """
+    global cam_thread
+    global message
+
+    if action == "on":
+        # Only do this if we haven't already.
+        if not cam_thread:
+            cam_thread = Thread(start_camera())
+            cam_thread.start()
+        message = "Camera is on."
+    elif action == "off":
+        GPIO.remove_event_detect(SENSOR_PIN)
+        GPIO.cleanup()
+        cam_thread = None
+        message = "Camera is off"
+    else:
+        message = "I don't know what you want me to do there."
+    return redirect("/")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
