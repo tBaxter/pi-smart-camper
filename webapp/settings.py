0898bbf87568f6fda2b9c73b1149387603f9fec3
TBaxter <mail.baxter@gmail.com>
2020-03-16 16:23:41 -0500

    corrected var

--- webapp/settings.py
+++ webapp/settings.py
@@ -5,7 +5,7 @@
 #PIR_POWER            = 2 # (red)
 #POWER_BUTTON_PIN     = 5 (white)
 #POWER_BUTTON_GROUND  = 6 (green)
-PIR_SENSOR_PIN        = 7 # (blue)
+PIR_SENSOR_PIN        = 4 # (blue - pin 7 - GPIO4 )
 #PIR_GROUND           = 9 # (black)
 #POWER_LED_PIN        = 12 (white)
 #POWER_LED_GROUND     = 14 (red)
